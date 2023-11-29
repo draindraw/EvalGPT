40a904f5306397c38eadb1a4ba179c58b5d9ef9c
keshavj029 <95275802+keshavj029@users.noreply.github.com>
2023-11-29 13:49:26 +0530

    Update critique.py

--- critique.py
+++ critique.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import os
 from pydantic import BaseModel
 from langchain.llms import GooglePalm
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 
 os.environ['GOOGLE_API_KEY'] = "AIzaSyAbhkn4KQxk8lBNtVEF3sNXV1e47SzO2Ic"
 
@@ -29,5 +38,5 @@ async def generate_text(data: InputData):
 
     response = title_chain({'idea' : data.idea})
 
-    return response["output"]
+    return {"content": response["output"]}
 
