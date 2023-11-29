d876bff18aef207e229bb00bd3e2be786acb53a7
keshavj029 <95275802+keshavj029@users.noreply.github.com>
2023-11-29 13:46:41 +0530

    Add files via upload

---
+++ critique.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import os
+from pydantic import BaseModel
+from langchain.llms import GooglePalm
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+
+app = FastAPI()
+
+os.environ['GOOGLE_API_KEY'] = "AIzaSyAbhkn4KQxk8lBNtVEF3sNXV1e47SzO2Ic"
+
+llm = GooglePalm(temperature = 0.3)
+
+
+class InputData(BaseModel):
+    idea: str
+
+@app.post("/predict")
+async def generate_text(data: InputData):
+
+    title_template = PromptTemplate(
+        input_variables = ['idea'],
+        template = "Act as a harsh critique for the following startup idea and provide feedback like what are the drawbacks and the strong points, also give tips to improve or add to the idea and based on the current market trends and different aspects provide a rough estimate of evaluation(in INR) : {idea} "
+
+    )
+
+    title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key = 'output')
+
+    response = title_chain({'idea' : data.idea})
+
+    return response["output"]
+
